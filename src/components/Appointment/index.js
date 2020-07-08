fb2c886ddeb5bd4b53341818a8262f5dce27d396
fsajadi <s.sfatemeh@gmail.com>
2020-07-08 12:30:21 -0400

    Implement the appontment component to use show component when an appointment is booked and to show the empty component when the apponitmnet is not booked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,19 @@
 import "components/Appointment/styles.scss";
+import Header from "components/Appointment/header";
+import Show from "components/Appointment/show";
+import Empty from "components/Appointment/empty";
 
 import React from "react";
 
 
 export default function Appointment(props) {
+
+ 
  return (
-   <h1></h1>
+   <div className="appointment">
+  <Header time={props.time} />
+  {props.interview ? <Show student={props.interview.student}  interviewer={props.interview.interviewer}  /> : <Empty />}
+   </div>
  );
  
 }
-

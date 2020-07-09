b56d13b1131ea20c97ae8db12e0ff387ebf833c2
fsajadi <s.sfatemeh@gmail.com>
2020-07-09 01:31:54 -0400

    Implement promise all to retrive both appointments and days data from the api

--- src/components/Application.js
+++ src/components/Application.js
@@ -55,16 +55,40 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = (day) => {
+    setState({
+      ...state,
+      day,
+    });
+  }
+
+  // const setDays = days => {
+  //   setState(prev => ({
+  //     ...prev, days
+  //   }))
+  // };
+
+ 
 
   useEffect(() => {
-    console.log(axios)
-    axios.get("http://localhost:8001/api/days")
-      .then(response => setDays(response.data))
-      .catch(error => console.log(error))
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments")
+    ])
+    .then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data}))
+    });
+    
   }, []);
-
 
   return (
     <main className="layout">
@@ -77,8 +101,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>

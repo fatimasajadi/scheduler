5f31a4507d76d2117b0c2b72450b4ee227532c92
fsajadi <s.sfatemeh@gmail.com>
2020-07-09 13:42:18 -0400

    Fetch appointment data and restrucre the retrived data to have appointmnets for a particukar day

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,60 +3,10 @@ import axios from "axios";
 import DayList from 'components/DayList.js';
 import Appointment from "components/Appointment";
 import "components/Application.scss";
-
-//Data
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-
-  {
-    id: 3,
-    time: "15pm",
-    interview: {
-      student: "Fatima Sajadi",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "16pm",
-  },
-  {
-    id: 4,
-    time: "17pm",
-    interview: {
-      student: "Michael Jackson",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-];
+import { getAppointmentsForDay } from "../helpers/selectors";
 
 
 export default function Application(props) {
-  // const [day, setDay] = useState("Monday");
-  // const [days, setDays] = useState([]);
 
   const [state, setState] = useState({
     day: "Monday",
@@ -69,6 +19,7 @@ export default function Application(props) {
       ...state,
       day,
     });
+
   }
 
   useEffect(() => {
@@ -76,10 +27,10 @@ export default function Application(props) {
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments")
     ])
-    .then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data}))
-    });
-    
+      .then((all) => {
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }))
+      });
+
   }, []);
 
   return (
@@ -106,15 +57,13 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {appointments.map((appointment) =>
+        {getAppointmentsForDay(state, state.day).map((appointment) =>
           <Appointment
             key={appointment.id}
             {...appointment}
           />)}
         <Appointment key="last" time="18pm" />
-
       </section>
-
     </main>
   );
 }
